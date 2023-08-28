73ca2908ce39c911489d49a9c648e25f6e8e84d3
Yehonatan Cohen Scaly <scalys7@gmail.com>
2023-08-28 12:47:45 +0300

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
 // Author: dWallet Labs, Ltd.
 // SPDX-License-Identifier: Apache-2.0
-mod group;
-mod traits;
 pub mod group;
 pub mod proofs;
+mod traits;
 use crypto_bigint::U128;
 
 /// A type alias for an unsigned integer of the size of the computation security parameter $\kappa$.
