e45837195f1da4bcfbbf10648f9ef80553b2e5e7
Yehonatan Cohen Scaly <scalys7@gmail.com>
2023-08-30 11:59:29 +0300

    proofs: `Error`, `Result`, `TranscriptProtocol` (#35)
    Co-authored-by: Zeev Manilovich <zeevmoney@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
-// Author: dWallet Labs, Ltd.
+// Author: dWallet Labs, LTD.
 // SPDX-License-Identifier: Apache-2.0
 pub mod group;
+pub mod proofs;
 mod traits;
+
+use crypto_bigint::U128;
+
+/// Represents an unsigned integer sized based on the computation security parameter, denoted as
+/// $\kappa$. Configured for 128-bit security using U128.
+pub type ComputationalSecuritySizedNumber = U128;
---
+++ src/proofs.rs
@@ -0,0 +1,48 @@
+// Author: dWallet Labs, LTD.
+// SPDX-License-Identifier: Apache-2.0
+use crypto_bigint::{Limb, Uint};
+use merlin::Transcript;
+use serde::Serialize;
+
+#[derive(thiserror::Error, Debug, PartialEq)]
+pub enum Error {
+    #[error("invalid parameters")]
+    InvalidParameters,
+
+    #[error("invalid proof - did not satisfy the verification equation")]
+    ProofVerificationError,
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+/// A transcript protocol for fiat-shamir transforms of interactive to non-interactive proofs.
+trait TranscriptProtocol {
+    fn serialize_to_transcript_as_json<T: Serialize>(
+        &mut self,
+        label: &'static [u8],
+        message: &T,
+    ) -> serde_json::Result<()>;
+
+    fn challenge<const LIMBS: usize>(&mut self, label: &'static [u8]) -> Uint<LIMBS>;
+}
+
+impl TranscriptProtocol for Transcript {
+    fn serialize_to_transcript_as_json<T: Serialize>(
+        &mut self,
+        label: &'static [u8],
+        message: &T,
+    ) -> serde_json::Result<()> {
+        let serialized_message = serde_json::to_vec(message)?;
+
+        self.append_message(label, serialized_message.as_slice());
+
+        Ok(())
+    }
+
+    fn challenge<const LIMBS: usize>(&mut self, label: &'static [u8]) -> Uint<LIMBS> {
+        let mut buf: Vec<u8> = vec![0u8; LIMBS * Limb::BYTES];
+        self.challenge_bytes(label, buf.as_mut_slice());
+
+        Uint::<LIMBS>::from_le_slice(&buf)
+    }
+}
