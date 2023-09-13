d662b433b0a07c8515d4d34ab2c57fe903576f5f
Yehonatan Cohen Scaly <scalys7@gmail.com>
2023-09-14 06:46:53 +0800

    tmp

--- src/ahe.rs
+++ src/ahe.rs
@@ -2,11 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crypto_bigint::{rand_core::CryptoRngCore, Uint};
-use tiresias::{DecryptionKey, EncryptionKey};
+use tiresias;
 
 use crate::group::GroupElement;
-
-mod paillier;
 
 /// An error in additively homomorphic encryption evaluation
 /// [`AdditivelyHomomorphicEncryption::evaluate_linear_transformation()`]
@@ -23,8 +21,13 @@ pub enum Error {
 /// [`AdditivelyHomomorphicEncryption::evaluate_linear_transformation()`]
 pub type Result<T> = std::result::Result<T, Error>;
 
-/// An Additively Homomorphic Encryption scheme
-pub trait AdditivelyHomomorphicEncryption<
+// todo: if I define non-honmorphic traits, then encryption + decryption may fail.
+// I can do something that fails, but then it'll be ugly and ambiguous with same names?
+
+// alt: define only for homomorphic, but still with traits for the keys themselves.
+
+/// An encryption key which allows encryption of messages
+pub trait EncryptionKey<
     const PLAINTEXT_LIMBS: usize,
     const RANDOMNESS_SPACE_SCALAR_LIMBS: usize,
     const CIPHERTEXT_SPACE_SCALAR_LIMBS: usize,
@@ -33,10 +36,7 @@ pub trait AdditivelyHomomorphicEncryption<
     CiphertextSpaceGroupElement: GroupElement<CIPHERTEXT_SPACE_SCALAR_LIMBS>,
 >
 {
-    type EncryptionKey;
-    type DecryptionKey;
-
-    /// $\Enc(pk, \pt; \eta_{\sf enc}) \to \ct$: Encrypt `plaintext` to `encryption_key` using
+    /// $\Enc(pk, \pt; \eta_{\sf enc}) \to \ct$: Encrypt `plaintext` to `self` using
     /// `randomness`.
     ///
     /// A deterministic algorithm that on input a public key $pk$, a plaintext $\pt \in \calP_{pk}$
@@ -45,65 +45,117 @@ pub trait AdditivelyHomomorphicEncryption<
     /// $\eta_{\sf enc} \in \calR_{pk}$ and then outputs $\ct=\Enc(pk, \pt; \eta_{\sf
     /// enc})\in\calC_{pk}$.
     fn encrypt_with_randomness(
-        encryption_key: &EncryptionKey,
+        &self,
         plaintext: Uint<PLAINTEXT_LIMBS>,
         randomness: &RandomnessSpaceGroupElement,
     ) -> CiphertextSpaceGroupElement;
 
-    // TODO: encrypt & decrypt as functions of types.
-
-    /// Encrypt `plaintext` to `encryption_key`.
+    /// Encrypt `plaintext` to `self`.
     ///
     /// This is the probabilistic encryption algorithm which samples randomness
     /// from `rng`.    
     fn encrypt(
-        encryption_key: &EncryptionKey,
+        &self,
         plaintext: Uint<PLAINTEXT_LIMBS>,
         rng: &mut impl CryptoRngCore,
     ) -> CiphertextSpaceGroupElement;
+}
 
+pub trait DecryptionKey<
+    const PLAINTEXT_LIMBS: usize,
+    const RANDOMNESS_SPACE_SCALAR_LIMBS: usize,
+    const CIPHERTEXT_SPACE_SCALAR_LIMBS: usize,
+    // TODO: MessageSpaceGroupElement with Z_n+ which is known-order. Then use this for eval.
+    RandomnessSpaceGroupElement: GroupElement<RANDOMNESS_SPACE_SCALAR_LIMBS>,
+    CiphertextSpaceGroupElement: GroupElement<CIPHERTEXT_SPACE_SCALAR_LIMBS>,
+>: EncryptionKey<PLAINTEXT_LIMBS, RANDOMNESS_SPACE_SCALAR_LIMBS, CIPHERTEXT_SPACE_SCALAR_LIMBS, RandomnessSpaceGroupElement, CiphertextSpaceGroupElement>
+{
     /// $\Dec(sk, \ct) \to \pt$: Decrypt `ciphertext` using `decryption_key`.
     /// A deterministic algorithm that on input a secret key $sk$ and a ciphertext $\ct \in
     /// \calC_{pk}$ outputs a plaintext $\pt \in \calP_{pk}$.
-    fn decrypt(
-        decryption_key: &DecryptionKey,
-        ciphertext: &CiphertextSpaceGroupElement,
-    ) -> Uint<PLAINTEXT_LIMBS>;
+    fn decrypt(&self, ciphertext: &CiphertextSpaceGroupElement) -> Uint<PLAINTEXT_LIMBS>;
+}
 
-    // TODO: option types
-
+/// An Additively Homomorphic Encryption scheme
+pub trait AdditivelyHomomorphicEncryption<
+    const PLAINTEXT_LIMBS: usize,
+    const RANDOMNESS_SPACE_SCALAR_LIMBS: usize,
+    const CIPHERTEXT_SPACE_SCALAR_LIMBS: usize,
+    // TODO: MessageSpaceGroupElement with Z_n+ which is known-order. Then use this for eval.
+    RandomnessSpaceGroupElement: GroupElement<RANDOMNESS_SPACE_SCALAR_LIMBS>,
+    CiphertextSpaceGroupElement: GroupElement<CIPHERTEXT_SPACE_SCALAR_LIMBS>,
+>: EncryptionKey<PLAINTEXT_LIMBS, RANDOMNESS_SPACE_SCALAR_LIMBS, CIPHERTEXT_SPACE_SCALAR_LIMBS, RandomnessSpaceGroupElement, CiphertextSpaceGroupElement>
+{
     /// $\Eval(pk,f, \ct_1,\ldots,\ct_t; \eta_{\sf eval})$: Efficient homomorphic evaluation of the
-    /// affine function defined by `free_variable` and `coefficients` on `ciphertexts`; to ensure
-    /// circuit-privacy, some implementations may require `mask` and `randmomness` to be `Some`.
-    ///
-    /// Whenever we omit the randomness in $\Eval(pk,
+    /// affine function defined by `free_variable` and `coefficients` on `ciphertexts`; to
+    /// ensure     circuit-privacy, some implementations may require `mask` and `randmomness` to
+    /// be `Some`.    Whenever we omit the randomness in $\Eval(pk,
     /// f, \ct_1,\ldots,\ct_t)$, we refer to the process of sampling a randomness $\eta_{\sf
     /// eval}\gets\{0,1\}^{\poly(\kappa)}$ and running $\Eval(pk, f, \ct_1,\ldots,\ct_t; \eta_{\sf
     /// eval})$. Given a public key $pk$, an affine function $f(x_1,\ldots,x_\ell)=
     /// a_0+\sum_{i=1}^{\ell}{a_i x_i}$ (with $a_i\in\ZZ$ and $\ell,\|a_i\|_2\in\poly(\kappa)$ for
-    /// all $0\le i\le \ell$) and $\ell$ ciphertexts $\ct_1,\ldots,\ct_t \in \calC$, $\Eval$ outputs
-    /// a ciphertext $\ct$.
+    /// all $0\le i\le \ell$) and $\ell$ ciphertexts $\ct_1,\ldots,\ct_t \in \calC$, $\Eval$
+    /// outputs a ciphertext $\ct$.
     fn evaluate_linear_transformation_with_randomness<
         const FUNCTION_DEGREE: usize,
         const COEFFICIENT_LIMBS: usize,
         const MASK_LIMBS: usize,
     >(
-        encryption_key: &EncryptionKey,
+        &self,
         free_variable: Uint<COEFFICIENT_LIMBS>,
         coefficients: [Uint<COEFFICIENT_LIMBS>; FUNCTION_DEGREE],
         ciphertexts: [CiphertextSpaceGroupElement; FUNCTION_DEGREE],
-        mask: Uint<MASK_LIMBS>,
-        randomness: &RandomnessSpaceGroupElement,
+        mask: Option<Uint<MASK_LIMBS>>,
+        randomness: Option<RandomnessSpaceGroupElement>,
     ) -> Result<CiphertextSpaceGroupElement>;
 
     fn evaluate_linear_transformation<
         const FUNCTION_DEGREE: usize,
         const COEFFICIENT_LIMBS: usize,
     >(
-        encryption_key: &EncryptionKey,
+        &self,
         free_variable: Uint<COEFFICIENT_LIMBS>,
         coefficients: [Uint<COEFFICIENT_LIMBS>; FUNCTION_DEGREE],
         ciphertexts: [CiphertextSpaceGroupElement; FUNCTION_DEGREE],
         rng: &mut impl CryptoRngCore,
     ) -> Result<CiphertextSpaceGroupElement>;
+}
+
+trait A {
+    fn foo() -> Option<u8>;
+}
+
+trait B: A {
+    fn foo() -> u8;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ahe;
+    use crate::ahe::B;
+
+    fn hey(x: u8) {}
+
+    fn you<T: B>(b: T) {
+        hey(b.foo())
+    }
+
+    struct Bla;
+    impl ahe::A for Bla {
+        fn foo() -> Option<u8> {
+            None
+        }
+    }
+
+    impl B for Bla {
+        fn foo() -> u8 {
+            42
+        }
+    }
+
+    #[test]
+    fn works() {
+        let bla = Bla{};
+        assert_eq!(bla.foo(), 42u8);
+    }
 }
