bfea91917bd72ec22985ccb130238d71bb67ea00
Yehonatan Cohen Scaly <scalys7@gmail.com>
2023-09-14 00:17:10 +0800

    comment

--- src/ahe.rs
+++ src/ahe.rs
@@ -23,6 +23,7 @@ pub enum Error {
 /// [`AdditivelyHomomorphicEncryption::evaluate_linear_transformation()`]
 pub type Result<T> = std::result::Result<T, Error>;
 
+/// An Additively Homomorphic Encryption scheme
 pub trait AdditivelyHomomorphicEncryption<
     const PLAINTEXT_LIMBS: usize,
     const RANDOMNESS_SPACE_SCALAR_LIMBS: usize,
@@ -35,23 +36,53 @@ pub trait AdditivelyHomomorphicEncryption<
     type EncryptionKey;
     type DecryptionKey;
 
+    /// $\Enc(pk, \pt; \eta_{\sf enc}) \to \ct$: Encrypt `plaintext` to `encryption_key` using
+    /// `randomness`.
+    ///
+    /// A deterministic algorithm that on input a public key $pk$, a plaintext $\pt \in \calP_{pk}$
+    /// and randomness $\eta_{\sf enc} \in \calR_{pk}$, outputs a ciphertext $\ct \in \calC_{pk}$.
+    /// We define $\Enc(pk, \pt)$ as a probabilistic algorithm that first uniformly samples
+    /// $\eta_{\sf enc} \in \calR_{pk}$ and then outputs $\ct=\Enc(pk, \pt; \eta_{\sf
+    /// enc})\in\calC_{pk}$.
     fn encrypt_with_randomness(
         encryption_key: &EncryptionKey,
         plaintext: Uint<PLAINTEXT_LIMBS>,
         randomness: &RandomnessSpaceGroupElement,
     ) -> CiphertextSpaceGroupElement;
 
+    // TODO: encrypt & decrypt as functions of types.
+
+    /// Encrypt `plaintext` to `encryption_key`.
+    ///
+    /// This is the probabilistic encryption algorithm which samples randomness
+    /// from `rng`.    
     fn encrypt(
         encryption_key: &EncryptionKey,
         plaintext: Uint<PLAINTEXT_LIMBS>,
         rng: &mut impl CryptoRngCore,
-    ) -> CiphertextSpaceGroupElement; // TODO: blanket implementation where we have Samplable?
+    ) -> CiphertextSpaceGroupElement;
 
+    /// $\Dec(sk, \ct) \to \pt$: Decrypt `ciphertext` using `decryption_key`.
+    /// A deterministic algorithm that on input a secret key $sk$ and a ciphertext $\ct \in
+    /// \calC_{pk}$ outputs a plaintext $\pt \in \calP_{pk}$.
     fn decrypt(
         decryption_key: &DecryptionKey,
         ciphertext: &CiphertextSpaceGroupElement,
     ) -> Uint<PLAINTEXT_LIMBS>;
 
+    // TODO: option types
+
+    /// $\Eval(pk,f, \ct_1,\ldots,\ct_t; \eta_{\sf eval})$: Efficient homomorphic evaluation of the
+    /// affine function defined by `free_variable` and `coefficients` on `ciphertexts`; to ensure
+    /// circuit-privacy, some implementations may require `mask` and `randmomness` to be `Some`.
+    ///
+    /// Whenever we omit the randomness in $\Eval(pk,
+    /// f, \ct_1,\ldots,\ct_t)$, we refer to the process of sampling a randomness $\eta_{\sf
+    /// eval}\gets\{0,1\}^{\poly(\kappa)}$ and running $\Eval(pk, f, \ct_1,\ldots,\ct_t; \eta_{\sf
+    /// eval})$. Given a public key $pk$, an affine function $f(x_1,\ldots,x_\ell)=
+    /// a_0+\sum_{i=1}^{\ell}{a_i x_i}$ (with $a_i\in\ZZ$ and $\ell,\|a_i\|_2\in\poly(\kappa)$ for
+    /// all $0\le i\le \ell$) and $\ell$ ciphertexts $\ct_1,\ldots,\ct_t \in \calC$, $\Eval$ outputs
+    /// a ciphertext $\ct$.
     fn evaluate_linear_transformation_with_randomness<
         const FUNCTION_DEGREE: usize,
         const COEFFICIENT_LIMBS: usize,
