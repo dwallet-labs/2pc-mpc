5e64fe01d25d4c532b53872c8c516c310e1a407e
Yehonatan Cohen Scaly <scalys7@gmail.com>
2023-09-16 07:52:20 +0900

    working with known-order group elements for plaintext of ahe

--- src/group/paillier.rs
+++ src/group/paillier.rs
@@ -3,15 +3,10 @@
 
 use tiresias::{LargeBiPrimeSizedNumber, PaillierModulusSizedNumber};
 
-use crate::group::{
-    additive_group_of_integers_modulu_n, multiplicative_group_of_integers_modulu_n,
-};
+use crate::group::multiplicative_group_of_integers_modulu_n;
 
 pub type CiphertextGroupElement =
     multiplicative_group_of_integers_modulu_n::GroupElement<{ PaillierModulusSizedNumber::LIMBS }>;
 
 pub type RandomnessGroupElement =
     multiplicative_group_of_integers_modulu_n::GroupElement<{ LargeBiPrimeSizedNumber::LIMBS }>;
-
-pub type MessageGroupElement =
-    additive_group_of_integers_modulu_n::GroupElement<{ LargeBiPrimeSizedNumber::LIMBS }>;
