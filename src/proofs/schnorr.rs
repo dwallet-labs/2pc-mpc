9fb56234fe2c77c115ec774de90a787c4ea685e6
Yehonatan Cohen Scaly <scalys7@gmail.com>
2023-09-17 03:04:07 +0900

    tmp

--- src/proofs/schnorr.rs
+++ src/proofs/schnorr.rs
@@ -5,8 +5,8 @@ pub mod commitment_of_discrete_log;
 
 pub mod knowledge_of_discrete_log;
 
-pub mod encryption_of_discrete_log;
 pub mod committed_affine_evaluation;
+pub mod encryption_of_discrete_log;
 pub mod knowledge_of_decommitment;
 
 use std::marker::PhantomData;
