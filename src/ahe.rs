ec4e811480cb337ee2edb22c19d12df4d8fbe90d
Yehonatan Cohen Scaly <scalys7@gmail.com>
2023-09-22 08:42:37 +0900

    comment

--- src/ahe.rs
+++ src/ahe.rs
@@ -73,19 +73,15 @@ pub trait AdditivelyHomomorphicEncryptionKey<
     ///
     /// A deterministic algorithm that on input a public key $pk$, a plaintext $\pt \in \calP_{pk}$
     /// and randomness $\eta_{\sf enc} \in \calR_{pk}$, outputs a ciphertext $\ct \in \calC_{pk}$.
-    /// We define $\Enc(pk, \pt)$ as a probabilistic algorithm that first uniformly samples
-    /// $\eta_{\sf enc} \in \calR_{pk}$ and then outputs $\ct=\Enc(pk, \pt; \eta_{\sf
-    /// enc})\in\calC_{pk}$.
     fn encrypt_with_randomness(
         &self,
         plaintext: &PlaintextSpaceGroupElement,
         randomness: &RandomnessSpaceGroupElement,
     ) -> CiphertextSpaceGroupElement;
 
-    /// Encrypt `plaintext` to `self`.
-    ///
-    /// This is the probabilistic encryption algorithm which samples randomness
-    /// from `rng`.    
+    /// $\Enc(pk, \pt)$: a probabilistic algorithm that first uniformly samples `randomness`
+    /// $\eta_{\sf enc} \in \calR_{pk}$ from `rng` and then calls [`Self::
+    /// encrypt_with_randomness()`] to encrypt `plaintext` to `self` using the sampled randomness.
     fn encrypt(
         &self,
         plaintext: &PlaintextSpaceGroupElement,
@@ -103,13 +99,53 @@ pub trait AdditivelyHomomorphicEncryptionKey<
     /// $\Eval(pk,f, \ct_1,\ldots,\ct_t; \eta_{\sf eval})$: Efficient homomorphic evaluation of the
     /// linear combination defined by `coefficients` and `ciphertexts`.
     ///
-    /// To ensure circuit-privacy, one must assure that `ciphertexts` are encryptions of plaintext
-    /// group elements (and thus their message is bounded by the plaintext group order) either by
-    /// generating them via ['Self::encrypt()'] or verifying appropriate zero-knowledge proofs from
-    /// encryptors.
+    /// In order to perform an affine evaluation, the free variable should be paired with an
+    /// encryption of one. If we wish to re-randomize the outputted ciphertext, this encryption of
+    /// one could use fresh randomness. Otherwise, randomness zero can be used.
     ///
-    /// To ensure circuit-privacy, the `mask` and `randmomness` to parameters may be used by
-    /// implementers.
+    /// SECURITY NOTICE: circuit-privacy is not assured by default. If circuit-privacy is required,
+    /// several steps must be carefully taken.
+    ///
+    /// 1. Rerandomization. This should be done by adding an encryption of zero with fresh
+    ///    randomness to the ciphertexts. In the case of an affine evaluation, this could be merged
+    ///    with the encryption of one added for the free variable, yielding a single encrytpion of
+    ///    one with fresh randomness that would be multiplied by the free variable.
+    ///
+    ///    In the (common) case in which the homomorphic evaluation should be done in a different
+    ///    group, two extra steps are required:
+    /// 2. Masking. Our evaluation should be masked by a random multiplication of the homomorphic
+    ///    evaluation group order $q$. This should be done by adding the masked multiplication to
+    ///    the free variable (taking it to be zero if unspecified.)
+    ///
+    ///    While the decryption modulo $q$
+    ///    will remain correct, assuming that the mask was "big enough", it will be statistically
+    ///    indistinguishable from random.
+    ///
+    ///    "Big enough" here means bigger by the statistical security parameter than the size of the
+    ///    evaluation.
+    ///
+    ///    Assuming a bound $B$ on both the coefficients and the (encrypted) messages, the
+    ///    evaluation is bounded by the number of coefficients $l$ by $B^2$.
+    ///
+    ///    In order to mask that, we need to add a mask that is bigger by the statistical security
+    ///    parameter. Since we multiply our mask by $q$, we need our mask to be of size $(l*B^2 / q)
+    ///   + s$.
+    ///
+    ///   Note that (unless we trust the encryptor) it is important to assure these bounds on
+    ///   the ciphertexts by verifying appropriate zero-knowledge proofs.
+    ///
+    ///    TODO: I wanted to say the coefficients are bounded to $q$ because we create them, but in
+    ///    fact when we prove in zero-knowledge that they are, we're going to have a gap here
+    ///    too right? and so the verifier should check we didn't go through modulation using
+    ///    that bound and not q.)
+    /// 3. No modulations. The size of our evaluation $2*l*B^2$ should be smaller than the order of
+    ///    the encryption plaintext group $N$ in order to assure it does not go through modulation
+    ///    in the plaintext space.
+    ///
+    /// TODO: can't I simply re-randomize the first ciphertext by adding an encryption of zero with
+    /// fresh randomness, instead of having to do these weird requirements that I can't enforce?
+    ///
+    /// a0 * E(x0; r1) => (a0 + w*q) * E(x0; r1 + r2) => E(a0*x0 + w*q*x0; r1 + r2).
     fn evaluate_linear_combination_with_randomness<const DIMENSION: usize>(
         &self,
         coefficients: &[PlaintextSpaceGroupElement; DIMENSION],
@@ -123,11 +159,6 @@ pub trait AdditivelyHomomorphicEncryptionKey<
     ///
     /// This is the probabilistic linear combination algorithm which samples `mask` and `randomness`
     /// from `rng` and calls [`Self::linear_combination_with_randomness()`].
-    ///
-    /// To ensure circuit-privacy, one must assure that `ciphertexts` are encryptions of plaintext
-    /// group elements (and thus their message is bounded by the plaintext group order) either by
-    /// generating them via ['Self::encrypt()'] or verifying appropriate zero-knowledge proofs from
-    /// encryptors.
     fn evaluate_linear_combination<const DIMENSION: usize>(
         &self,
         coefficients: &[PlaintextSpaceGroupElement; DIMENSION],
