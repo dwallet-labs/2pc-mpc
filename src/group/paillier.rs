b84f19defbdf776cc6a77dac2081e3173463aa3b
Yehonatan Cohen Scaly <scalys7@gmail.com>
2023-09-22 20:40:52 +0900

    tmp

--- src/group/paillier.rs
+++ src/group/paillier.rs
@@ -1,12 +1,9 @@
 // Author: dWallet Labs, LTD.
 // SPDX-License-Identifier: Apache-2.0
 
-use crypto_bigint::modular::runtime_mod::DynResidue;
 use tiresias::{LargeBiPrimeSizedNumber, PaillierModulusSizedNumber};
 
 use crate::group::multiplicative_group_of_integers_modulu_n;
-
-pub type PlaintextGroupElement = DynResidue<{ LargeBiPrimeSizedNumber::LIMBS }>;
 
 pub type CiphertextGroupElement =
     multiplicative_group_of_integers_modulu_n::GroupElement<{ PaillierModulusSizedNumber::LIMBS }>;
