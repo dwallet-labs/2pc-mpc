70c6f4c0ee7a287f69b96cde46117ad7820707a8
Yehonatan Cohen Scaly <scalys7@gmail.com>
2023-11-27 19:23:44 +0000

    first proofs

--- src/presign/decentralized_party/nonce_shares_and_masked_key_shares_commitment_round.rs
+++ src/presign/decentralized_party/nonce_shares_and_masked_key_shares_commitment_round.rs
@@ -114,7 +114,7 @@ where
         RangeProof,
     >: From<enhanced::ConstrainedWitnessValue<RANGE_CLAIMS_PER_SCALAR, WITNESS_MASK_LIMBS>>,
 {
-    pub fn commit_signature_nonce_share_and_masked_encrypted_secret_key_share(
+    pub fn commit_signature_nonce_shares_and_masked_encrypted_secret_key_shares(
         self,
         centralized_party_nonce_shares_commitments_and_batched_proof: SignatureNonceSharesCommitmentsAndBatchedProof<
             SCALAR_LIMBS,
--- src/presign/decentralized_party/nonce_shares_and_masked_key_shares_proof_share_round.rs
+++ src/presign/decentralized_party/nonce_shares_and_masked_key_shares_proof_share_round.rs
@@ -1,2 +1,276 @@
 // Author: dWallet Labs, LTD.
 // SPDX-License-Identifier: Apache-2.0
+
+use core::array;
+use std::collections::HashMap;
+
+use crypto_bigint::{rand_core::CryptoRngCore, Encoding, Uint};
+use serde::Serialize;
+
+use crate::{
+    ahe, commitments,
+    commitments::GroupsPublicParametersAccessors as _,
+    dkg::decentralized_party::decommitment_round,
+    group,
+    group::{
+        additive_group_of_integers_modulu_n::power_of_two_moduli, GroupElement as _,
+        PrimeGroupElement, Samplable,
+    },
+    presign::decentralized_party::{
+        masked_nonces_commitment_round, nonce_shares_and_masked_key_shares_decommitment_round,
+    },
+    proofs,
+    proofs::{
+        range,
+        range::CommitmentPublicParametersAccessor as _,
+        schnorr::{
+            encryption_of_discrete_log, encryption_of_tuple,
+            knowledge_of_decommitment::LanguageCommitmentScheme, language::enhanced,
+        },
+    },
+    AdditivelyHomomorphicEncryptionKey, Commitment, PartyID,
+};
+
+#[cfg_attr(feature = "benchmarking", derive(Clone))]
+pub struct Party<
+    const SCALAR_LIMBS: usize,
+    const RANGE_PROOF_COMMITMENT_SCHEME_MESSAGE_SPACE_SCALAR_LIMBS: usize,
+    const RANGE_CLAIMS_PER_SCALAR: usize,
+    const RANGE_CLAIM_LIMBS: usize,
+    const WITNESS_MASK_LIMBS: usize,
+    const PLAINTEXT_SPACE_SCALAR_LIMBS: usize,
+    GroupElement: PrimeGroupElement<SCALAR_LIMBS>,
+    EncryptionKey: AdditivelyHomomorphicEncryptionKey<PLAINTEXT_SPACE_SCALAR_LIMBS>,
+    RangeProof: proofs::RangeProof<RANGE_PROOF_COMMITMENT_SCHEME_MESSAGE_SPACE_SCALAR_LIMBS, RANGE_CLAIM_LIMBS>,
+    CommitmentScheme: LanguageCommitmentScheme<SCALAR_LIMBS, GroupElement::Scalar, GroupElement>,
+    ProtocolContext: Clone + Serialize,
+> where
+    Uint<RANGE_CLAIM_LIMBS>: Encoding,
+    Uint<WITNESS_MASK_LIMBS>: Encoding,
+    group::ScalarValue<SCALAR_LIMBS, GroupElement>: From<Uint<SCALAR_LIMBS>>,
+    range::CommitmentSchemeMessageSpaceValue<
+        RANGE_PROOF_COMMITMENT_SCHEME_MESSAGE_SPACE_SCALAR_LIMBS,
+        RANGE_CLAIMS_PER_SCALAR,
+        RANGE_CLAIM_LIMBS,
+        RangeProof,
+    >: From<enhanced::ConstrainedWitnessValue<RANGE_CLAIMS_PER_SCALAR, WITNESS_MASK_LIMBS>>,
+{
+    pub(super) party_id: PartyID,
+    pub(super) threshold: PartyID,
+    pub(super) number_of_parties: PartyID,
+    // TODO: should we get this like that?
+    pub(super) protocol_context: ProtocolContext,
+    pub(super) group_public_parameters: GroupElement::PublicParameters,
+    pub(super) scalar_group_public_parameters: group::PublicParameters<GroupElement::Scalar>,
+    pub(super) encryption_scheme_public_parameters: EncryptionKey::PublicParameters,
+    pub(super) commitment_scheme_public_parameters: CommitmentScheme::PublicParameters,
+    pub(super) range_proof_public_parameters: RangeProof::PublicParameters<RANGE_CLAIMS_PER_SCALAR>,
+    pub(super) public_key_share: GroupElement,
+    pub(super) public_key: GroupElement,
+    pub(super) encryption_of_secret_key_share: EncryptionKey::CiphertextSpaceGroupElement,
+    pub(super) centralized_party_public_key_share: GroupElement,
+    pub(super) shares_of_decentralized_party_signature_nonce_shares: Vec<GroupElement::Scalar>,
+    pub(super) encryption_of_share_of_decentralized_party_signature_nonce_shares_randomness:
+        Vec<EncryptionKey::RandomnessSpaceGroupElement>,
+    pub(super) encryption_of_signature_nonce_shares_proof_share_round_party:
+        encryption_of_discrete_log::ProofAggregationProofShareRoundParty<
+            SCALAR_LIMBS,
+            RANGE_PROOF_COMMITMENT_SCHEME_MESSAGE_SPACE_SCALAR_LIMBS,
+            RANGE_CLAIMS_PER_SCALAR,
+            RANGE_CLAIM_LIMBS,
+            WITNESS_MASK_LIMBS,
+            PLAINTEXT_SPACE_SCALAR_LIMBS,
+            GroupElement::Scalar,
+            GroupElement,
+            EncryptionKey,
+            RangeProof,
+            ProtocolContext,
+        >,
+    pub(super) masked_encryptions_of_decentralized_party_secret_key_shares_proof_share_round_party:
+        encryption_of_tuple::ProofAggregationProofShareRoundParty<
+            SCALAR_LIMBS,
+            RANGE_PROOF_COMMITMENT_SCHEME_MESSAGE_SPACE_SCALAR_LIMBS,
+            RANGE_CLAIMS_PER_SCALAR,
+            RANGE_CLAIM_LIMBS,
+            WITNESS_MASK_LIMBS,
+            PLAINTEXT_SPACE_SCALAR_LIMBS,
+            GroupElement::Scalar,
+            GroupElement,
+            EncryptionKey,
+            RangeProof,
+            ProtocolContext,
+        >,
+}
+
+impl<
+        const SCALAR_LIMBS: usize,
+        const RANGE_PROOF_COMMITMENT_SCHEME_MESSAGE_SPACE_SCALAR_LIMBS: usize,
+        const RANGE_CLAIMS_PER_SCALAR: usize,
+        const RANGE_CLAIM_LIMBS: usize,
+        const WITNESS_MASK_LIMBS: usize,
+        const PLAINTEXT_SPACE_SCALAR_LIMBS: usize,
+        GroupElement: PrimeGroupElement<SCALAR_LIMBS>,
+        EncryptionKey: AdditivelyHomomorphicEncryptionKey<PLAINTEXT_SPACE_SCALAR_LIMBS>,
+        RangeProof: proofs::RangeProof<
+            RANGE_PROOF_COMMITMENT_SCHEME_MESSAGE_SPACE_SCALAR_LIMBS,
+            RANGE_CLAIM_LIMBS,
+        >,
+        CommitmentScheme: LanguageCommitmentScheme<SCALAR_LIMBS, GroupElement::Scalar, GroupElement>,
+        ProtocolContext: Clone + Serialize,
+    >
+    Party<
+        SCALAR_LIMBS,
+        RANGE_PROOF_COMMITMENT_SCHEME_MESSAGE_SPACE_SCALAR_LIMBS,
+        RANGE_CLAIMS_PER_SCALAR,
+        RANGE_CLAIM_LIMBS,
+        WITNESS_MASK_LIMBS,
+        PLAINTEXT_SPACE_SCALAR_LIMBS,
+        GroupElement,
+        EncryptionKey,
+        RangeProof,
+        CommitmentScheme,
+        ProtocolContext,
+    >
+where
+    Uint<RANGE_CLAIM_LIMBS>: Encoding,
+    Uint<WITNESS_MASK_LIMBS>: Encoding,
+    group::ScalarValue<SCALAR_LIMBS, GroupElement>: From<Uint<SCALAR_LIMBS>>,
+    range::CommitmentSchemeMessageSpaceValue<
+        RANGE_PROOF_COMMITMENT_SCHEME_MESSAGE_SPACE_SCALAR_LIMBS,
+        RANGE_CLAIMS_PER_SCALAR,
+        RANGE_CLAIM_LIMBS,
+        RangeProof,
+    >: From<enhanced::ConstrainedWitnessValue<RANGE_CLAIMS_PER_SCALAR, WITNESS_MASK_LIMBS>>,
+{
+    pub fn generate_signature_nonce_shares_and_masked_encrypted_secret_key_shares_proof_shares(
+        self,
+        encryption_of_signature_nonce_shares_decommitments: HashMap<
+            PartyID,
+            encryption_of_discrete_log::Decommitment<
+                SCALAR_LIMBS,
+                RANGE_PROOF_COMMITMENT_SCHEME_MESSAGE_SPACE_SCALAR_LIMBS,
+                RANGE_CLAIMS_PER_SCALAR,
+                RANGE_CLAIM_LIMBS,
+                WITNESS_MASK_LIMBS,
+                PLAINTEXT_SPACE_SCALAR_LIMBS,
+                GroupElement::Scalar,
+                GroupElement,
+                EncryptionKey,
+                RangeProof,
+            >,
+        >,
+        masked_encryptions_of_decentralized_party_secret_key_shares_decommitments: HashMap<
+            PartyID,
+            encryption_of_tuple::Decommitment<
+                SCALAR_LIMBS,
+                RANGE_PROOF_COMMITMENT_SCHEME_MESSAGE_SPACE_SCALAR_LIMBS,
+                RANGE_CLAIMS_PER_SCALAR,
+                RANGE_CLAIM_LIMBS,
+                WITNESS_MASK_LIMBS,
+                PLAINTEXT_SPACE_SCALAR_LIMBS,
+                GroupElement::Scalar,
+                GroupElement,
+                EncryptionKey,
+                RangeProof,
+            >,
+        >,
+    ) -> crate::Result<(
+        (
+            encryption_of_discrete_log::ProofShare<
+                SCALAR_LIMBS,
+                RANGE_PROOF_COMMITMENT_SCHEME_MESSAGE_SPACE_SCALAR_LIMBS,
+                RANGE_CLAIMS_PER_SCALAR,
+                RANGE_CLAIM_LIMBS,
+                WITNESS_MASK_LIMBS,
+                PLAINTEXT_SPACE_SCALAR_LIMBS,
+                GroupElement::Scalar,
+                GroupElement,
+                EncryptionKey,
+                RangeProof,
+            >,
+            encryption_of_tuple::ProofShare<
+                SCALAR_LIMBS,
+                RANGE_PROOF_COMMITMENT_SCHEME_MESSAGE_SPACE_SCALAR_LIMBS,
+                RANGE_CLAIMS_PER_SCALAR,
+                RANGE_CLAIM_LIMBS,
+                WITNESS_MASK_LIMBS,
+                PLAINTEXT_SPACE_SCALAR_LIMBS,
+                GroupElement::Scalar,
+                GroupElement,
+                EncryptionKey,
+                RangeProof,
+            >,
+        ),
+        masked_nonces_commitment_round::Party<
+            SCALAR_LIMBS,
+            RANGE_PROOF_COMMITMENT_SCHEME_MESSAGE_SPACE_SCALAR_LIMBS,
+            RANGE_CLAIMS_PER_SCALAR,
+            RANGE_CLAIM_LIMBS,
+            WITNESS_MASK_LIMBS,
+            PLAINTEXT_SPACE_SCALAR_LIMBS,
+            GroupElement,
+            EncryptionKey,
+            RangeProof,
+            CommitmentScheme,
+            ProtocolContext,
+        >,
+    )> {
+        let (
+            encryption_of_signature_nonce_shares_proof_share,
+            encryption_of_signature_nonce_shares_proof_aggregation_round_party,
+        ) = self
+            .encryption_of_signature_nonce_shares_proof_share_round_party
+            .generate_proof_share(encryption_of_signature_nonce_shares_decommitments)?;
+
+        let (
+            masked_encryptions_of_decentralized_party_secret_key_shares_proof_share,
+            masked_encryptions_of_decentralized_party_secret_key_shares_proof_aggregation_round_party,
+        ) = self
+            .masked_encryptions_of_decentralized_party_secret_key_shares_proof_share_round_party
+            .generate_proof_share(
+                masked_encryptions_of_decentralized_party_secret_key_shares_decommitments,
+            )?;
+
+        let party = masked_nonces_commitment_round::Party::<
+            SCALAR_LIMBS,
+            RANGE_PROOF_COMMITMENT_SCHEME_MESSAGE_SPACE_SCALAR_LIMBS,
+            RANGE_CLAIMS_PER_SCALAR,
+            RANGE_CLAIM_LIMBS,
+            WITNESS_MASK_LIMBS,
+            PLAINTEXT_SPACE_SCALAR_LIMBS,
+            GroupElement,
+            EncryptionKey,
+            RangeProof,
+            CommitmentScheme,
+            ProtocolContext,
+        > {
+            party_id: self.party_id,
+            threshold: self.threshold,
+            number_of_parties: self.number_of_parties,
+            protocol_context: self.protocol_context,
+            group_public_parameters: self.group_public_parameters,
+            scalar_group_public_parameters: self.scalar_group_public_parameters,
+            encryption_scheme_public_parameters: self.encryption_scheme_public_parameters,
+            commitment_scheme_public_parameters: self.commitment_scheme_public_parameters,
+            range_proof_public_parameters: self.range_proof_public_parameters,
+            public_key_share: self.public_key_share,
+            public_key: self.public_key,
+            encryption_of_secret_key_share: self.encryption_of_secret_key_share,
+            centralized_party_public_key_share: self.centralized_party_public_key_share,
+            shares_of_decentralized_party_signature_nonce_shares: self
+                .shares_of_decentralized_party_signature_nonce_shares,
+            encryption_of_share_of_decentralized_party_signature_nonce_shares_randomness: self
+                .encryption_of_share_of_decentralized_party_signature_nonce_shares_randomness,
+            encryption_of_signature_nonce_shares_proof_aggregation_round_party,
+            masked_encryptions_of_decentralized_party_secret_key_shares_proof_aggregation_round_party,
+        };
+
+        Ok((
+            (
+                encryption_of_signature_nonce_shares_proof_share,
+                masked_encryptions_of_decentralized_party_secret_key_shares_proof_share,
+            ),
+            party,
+        ))
+    }
+}
