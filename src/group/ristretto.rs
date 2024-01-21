b1a0d4b516b67643f5f211218e343b3e64ea23cd
Yehonatan Cohen Scaly <scalys7@gmail.com>
2024-01-21 14:32:15 +0200

    update doc

--- src/group/ristretto.rs
+++ src/group/ristretto.rs
@@ -22,7 +22,7 @@ pub const MODULUS: U256 =
 
 /// Any Montgomery elliptic curve can be represented as an equation in the following template:
 /// $By^{2}=x^{3}+Ax^{2}+x} mod(p)$. For ristretto specifically, $A = 486662$ and $B = 1$, yielding
-/// the equation $y^2 = x^3 + 7 mod(p)$.
+/// the equation $y^2 = x^3 + 486662x^2 + x mod(p)$.
 pub const CURVE_EQUATION_A: U256 =
     U256::from_be_hex("0000000000000000000000000000000000000000000000000000000000076d06");
 pub const CURVE_EQUATION_B: U256 = U256::ONE;
