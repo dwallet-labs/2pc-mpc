552bc867c8a08e6090f96984f2ba4bcb752efc23
Erik Takke <erik.takke@3milabs.tech>
2024-05-24 11:54:48 +0200

    Annotate Sign > Decentralized > Signature partial decryption round

--- src/sign/decentralized_party/signature_partial_decryption_round.rs
+++ src/sign/decentralized_party/signature_partial_decryption_round.rs
@@ -212,32 +212,39 @@ where
             rng,
         )?;
 
+        // = ct_A
         let encrypted_partial_signature = EncryptionKey::CiphertextSpaceGroupElement::new(
             public_nonce_encrypted_partial_signature_and_proof.encrypted_partial_signature,
             self.encryption_scheme_public_parameters
                 .ciphertext_space_public_parameters(),
         )?;
 
+        // = R
         let public_nonce = GroupElement::new(
             public_nonce_encrypted_partial_signature_and_proof.public_nonce,
             &self.group_public_parameters,
-        )?; // $R$
+        )?;
 
-        let nonce_x_coordinate = public_nonce.x(); // $r$
+        // = r
+        let nonce_x_coordinate = public_nonce.x();
 
+        // === Compute pt_A ===
+        // Protocol 6, step 2c
         let partial_signature_decryption_share = Option::from(
             self.decryption_key_share
                 .generate_decryption_share_semi_honest(
-                    &encrypted_partial_signature,
+                    &encrypted_partial_signature, // = ct_A
                     &self.decryption_key_share_public_parameters,
                 ),
         )
         .ok_or(Error::InternalError)?;
 
+        // === Compute pt_4 ===
+        // Protocol 6, step 2c
         let masked_nonce_decryption_share = Option::from(
             self.decryption_key_share
                 .generate_decryption_share_semi_honest(
-                    &self.encrypted_masked_nonce_share,
+                    &self.encrypted_masked_nonce_share, // = ct_4
                     &self.decryption_key_share_public_parameters,
                 ),
         )
@@ -262,6 +269,9 @@ where
         ))
     }
 
+    /// This function implements Protocol 6, step 2a of the
+    /// 2PC-MPC: Emulating Two Party ECDSA in Large-Scale MPC paper.
+    /// src: https://eprint.iacr.org/2024/253
     #[allow(clippy::too_many_arguments)]
     fn verify_encrypted_signature_parts_prehash_inner(
         message: GroupElement::Scalar,
@@ -320,19 +330,21 @@ where
         centralized_party_nonce_share_commitment: GroupElement,
         rng: &mut impl CryptoRngCore,
     ) -> crate::Result<()> {
+        // = R
         let public_nonce = GroupElement::new(
             public_nonce_encrypted_partial_signature_and_proof.public_nonce,
             group_public_parameters,
-        )?; // $R$
+        )?;
 
-        let nonce_x_coordinate = public_nonce.x(); // $r$
+        // = r
+        let nonce_x_coordinate = public_nonce.x();
 
+        // Gather DComDL public parameters
         let commitment_scheme_public_parameters =
             pedersen::PublicParameters::derive::<SCALAR_LIMBS, GroupElement>(
                 scalar_group_public_parameters.clone(),
                 group_public_parameters.clone(),
             )?;
-
         let language_public_parameters = committment_of_discrete_log::PublicParameters::new::<
             SCALAR_LIMBS,
             GroupElement::Scalar,
@@ -345,18 +357,21 @@ where
             public_nonce_encrypted_partial_signature_and_proof.public_nonce,
         );
 
+        // === Verify DComDL proof ===
+        // Protocol 6, step 2a, dash 1
         public_nonce_encrypted_partial_signature_and_proof
             .public_nonce_proof
             .verify(
                 protocol_context,
                 &language_public_parameters,
                 vec![[
-                    centralized_party_nonce_share_commitment.clone(),
-                    nonce_public_share.clone(),
+                    centralized_party_nonce_share_commitment.clone(), // = K_A
+                    nonce_public_share.clone(),                       // = R_B
                 ]
                 .into()],
             )?;
 
+        // Gather DComRatio language parameters
         let language_public_parameters =
             discrete_log_ratio_of_committed_values::PublicParameters::new::<
                 SCALAR_LIMBS,
@@ -369,30 +384,33 @@ where
                 centralized_party_public_key_share.clone(),
             );
 
+        // = U_A
         let nonce_share_by_key_share_commitment = GroupElement::new(
             public_nonce_encrypted_partial_signature_and_proof.nonce_share_by_key_share_commitment,
             group_public_parameters,
         )?;
 
+        // === Verify DComRatio proof ===
+        // Protocol 6, step 2a, dash 2
         public_nonce_encrypted_partial_signature_and_proof
             .nonce_share_by_key_share_proof
             .verify(
                 protocol_context,
                 &language_public_parameters,
                 vec![[
-                    centralized_party_nonce_share_commitment.clone(),
-                    nonce_share_by_key_share_commitment.clone(),
+                    centralized_party_nonce_share_commitment.clone(), // = K_A
+                    nonce_share_by_key_share_commitment.clone(),      // = U_A
                 ]
                 .into()],
             )?;
 
+        // Generate DComEval language parameters
         let encrypted_mask_upper_bound = composed_witness_upper_bound::<
             RANGE_CLAIMS_PER_SCALAR,
             PLAINTEXT_SPACE_SCALAR_LIMBS,
             COMMITMENT_SCHEME_MESSAGE_SPACE_SCALAR_LIMBS,
             RangeProof,
         >()?;
-
         let encrypted_masked_key_share_upper_bound: Option<_> = composed_witness_upper_bound::<
             RANGE_CLAIMS_PER_SCALAR,
             PLAINTEXT_SPACE_SCALAR_LIMBS,
@@ -401,18 +419,15 @@ where
         >()?
         .checked_mul(&encrypted_mask_upper_bound)
         .into();
-
         let ciphertexts_and_upper_bounds = [
-            (encrypted_mask.clone(), encrypted_mask_upper_bound),
+            (encrypted_mask.clone(), encrypted_mask_upper_bound), // = (ct_1, ...)
             (
-                encrypted_masked_key_share.clone(),
+                encrypted_masked_key_share.clone(), // = ct_2
                 encrypted_masked_key_share_upper_bound.ok_or(Error::InvalidPublicParameters)?,
             ),
         ]
         .map(|(ct, upper_bound)| (ct.value(), upper_bound));
-
         let commitment_scheme_public_parameters = commitment_scheme_public_parameters.into();
-
         let language_public_parameters = committed_linear_evaluation::PublicParameters::<
             PLAINTEXT_SPACE_SCALAR_LIMBS,
             SCALAR_LIMBS,
@@ -426,7 +441,6 @@ where
             commitment_scheme_public_parameters,
             ciphertexts_and_upper_bounds,
         );
-
         let language_public_parameters = EnhancedPublicParameters::<
             SOUND_PROOFS_REPETITIONS,
             NUM_RANGE_CLAIMS,
@@ -460,11 +474,14 @@ where
             language_public_parameters,
         )?;
 
+        // ct_A
         let encrypted_partial_signature = EncryptionKey::CiphertextSpaceGroupElement::new(
             public_nonce_encrypted_partial_signature_and_proof.encrypted_partial_signature,
             encryption_scheme_public_parameters.ciphertext_space_public_parameters(),
         )?;
 
+        // === Verify DComEval proof ===
+        // Protocol 6, step 2a, dash 3
         let range_proof_commitment = proof::range::CommitmentSchemeCommitmentSpaceGroupElement::<
             COMMITMENT_SCHEME_MESSAGE_SPACE_SCALAR_LIMBS,
             NUM_RANGE_CLAIMS,
@@ -476,7 +493,6 @@ where
                 .commitment_scheme_public_parameters()
                 .commitment_space_public_parameters(),
         )?;
-
         public_nonce_encrypted_partial_signature_and_proof
             .encrypted_partial_signature_proof
             .verify(
@@ -580,14 +596,17 @@ where
             group_public_parameters,
         )?;
 
+        // = R_B
         let nonce_public_share =
             GroupElement::new(presign.nonce_public_share, group_public_parameters)?;
 
+        // = ct_1
         let encrypted_mask = EncryptionKey::CiphertextSpaceGroupElement::new(
             presign.encrypted_mask,
             encryption_scheme_public_parameters.ciphertext_space_public_parameters(),
         )?;
 
+        // = ct_2
         let encrypted_masked_key_share = EncryptionKey::CiphertextSpaceGroupElement::new(
             presign.encrypted_masked_key_share,
             encryption_scheme_public_parameters.ciphertext_space_public_parameters(),
@@ -659,19 +678,23 @@ where
             &group_public_parameters,
         )?;
 
+        // = R_B
         let nonce_public_share =
             GroupElement::new(presign.nonce_public_share, &group_public_parameters)?;
 
+        // = ct_1
         let encrypted_mask = EncryptionKey::CiphertextSpaceGroupElement::new(
             presign.encrypted_mask,
             encryption_scheme_public_parameters.ciphertext_space_public_parameters(),
         )?;
 
+        // = ct_2
         let encrypted_masked_key_share = EncryptionKey::CiphertextSpaceGroupElement::new(
             presign.encrypted_masked_key_share,
             encryption_scheme_public_parameters.ciphertext_space_public_parameters(),
         )?;
 
+        // = ct_4
         let encrypted_masked_nonce_share = EncryptionKey::CiphertextSpaceGroupElement::new(
             presign.encrypted_masked_nonce_share,
             encryption_scheme_public_parameters.ciphertext_space_public_parameters(),
