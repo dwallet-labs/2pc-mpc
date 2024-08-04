d1091562d758544b69bf4857b42b16a6a93fc1cc
Itay Levy <itaylevy134@gmail.com>
2024-08-04 18:07:54 +0300

    renamed function

--- src/sign/decentralized_party/signature_threshold_decryption_round.rs
+++ src/sign/decentralized_party/signature_threshold_decryption_round.rs
@@ -156,13 +156,13 @@ where
         Ok((self.nonce_x_coordinate, signature_s))
     }
 
-    /// A wrapper function for [`Self::verify_decrypted_signature_internal`] that uses self's
+    /// A wrapper function for [`Self::verify_decrypted_signature_wrapper`] that uses self's
     /// attributes to verify the decrypted signature.
     pub fn verify_decrypted_signature(
         self,
         signature_s: GroupElement::Scalar,
     ) -> crate::Result<(GroupElement::Scalar, GroupElement::Scalar)> {
-        Self::verify_decrypted_signature_internal(
+        Self::verify_decrypted_signature_wrapper(
             self.nonce_x_coordinate,
             signature_s,
             self.message,
@@ -173,7 +173,7 @@ where
     /// The lightweight $$ O(1) $$ threshold decryption logic, which simply verifies the output of
     /// the decryption sent by the designated decrypting party. Returns a [`Error::MaliciousDesignatedDecryptingParty`]
     /// in case of an invalid signature, and accepts otherwise.
-    pub fn verify_decrypted_signature_internal(
+    pub fn verify_decrypted_signature_wrapper(
         nonce_x_coordinate: GroupElement::Scalar,
         signature_s: GroupElement::Scalar,
         message: GroupElement::Scalar,
