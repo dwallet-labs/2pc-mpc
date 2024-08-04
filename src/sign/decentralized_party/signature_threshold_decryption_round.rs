8eccf511fc11fa7c7c9f5660a8076be3ec9ffdcb
Itay Levy <itaylevy134@gmail.com>
2024-08-04 12:11:48 +0300

    fixed doc

--- src/sign/decentralized_party/signature_threshold_decryption_round.rs
+++ src/sign/decentralized_party/signature_threshold_decryption_round.rs
@@ -171,7 +171,7 @@ where
     }
 
     /// The lightweight $$ O(1) $$ threshold decryption logic, which simply verifies the output of
-    /// the decryption sent by the designated decrypting party. Returns a [`Error::SignatureVerification`]
+    /// the decryption sent by the designated decrypting party. Returns a [`Error::MaliciousDesignatedDecryptingParty`]
     /// in case of an invalid signature, and accepts otherwise.
     pub fn verify_decrypted_signature_internal(
         nonce_x_coordinate: GroupElement::Scalar,
